60c45e1ca0cc9fc0d96695cdc11aca16422e882c
acmesds@somemail.com <acmesds@nowhere.org>
2018-03-01 16:30:47 -0600

    updatefile hashing

--- hack.sh
+++ hack.sh
@@ -5,22 +5,37 @@ case "$1." in
 
 flatten.)
 	rm _hash
+	rm _links
+	rm _flatten.sh
+	rm _expand.sh
 	rm _make.sh
-	mkdir flat
+	let block=0
+	let maxfiles=450
+	let files=0
+	mkdir flat$block
 	source hack.sh $2
-	sed -E "s/([^ ]*) \|\| ([^ ]*)/cp \2 flat\/\1/" _hash >_flatten.sh
-	sed -E "s/([^ ]*) \|\| ([^ ]*)/cp -n flat\/\1* \2/" _hash >_expand.sh
+	sed -E "s/([^ ]*) \|\| ([^ ]*)/cp \2 \1/" _hash >_flatten.sh
+	sed -E "s/([^ ]*) \|\| ([^ ]*)/cp -n \1* \2/" _hash >_expand.sh
 	echo "run _flatten, then _make, then _expand"
 	;;
 
 *)
-	echo "hacking $1"
+	echo "scan $1 block=$block files=$files max=$maxfiles"
 	echo "mkdir $1" >> _make.sh
 	for file in $1/*; do
-		if [ -d "$file" ]; then
-			source hack.sh $file
-		else
-			echo "$(echo "$file" | md5sum | cut -d' ' -f1) || $file" >> _hash
+		if [ -L "$file" ]; then   # ignore symbolic links
+			echo "ignoring $file" >> _links			
+		elif [ -d "$file" ]; then  # recurse directories
+			. hack.sh $file
+		else   # add file to copy list
+			echo "flat$block/$(echo "$file" | md5sum | cut -d' ' -f1) || $file" >> _hash
+			let files=files+1
+			if [ $files -gt $maxfiles ]; then
+				let block=block+1
+				let files=0
+				mkdir flat$block
+				echo "mkdir flat$block" >> _make.sh
+			fi
 		fi
 	done
 	;;
