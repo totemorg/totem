8fdd2985f49edff93d5dc1f317b62e0fb15cacb9
acmesds@somemail.com <acmesds@nowhere.org>
2018-05-01 17:17:05 -0500

    atom matlab fixes

--- config.sh
+++ config.sh
@@ -70,7 +70,8 @@ source pass.sh
 # define server domains
 export SERVICE_NAME=Totem1
 export SERVICE_MASTER_URL=http://localhost:8080
-export SERVICE_WORKER_URL=https://localhost:8443
+export SERVICE_WORKER_URL=http://localhost:8080
+#export SERVICE_WORKER_URL=https://localhost:8443
 #export SERVICE_WORKER_URL=http://localhost:8081  # in debug mode
 
 # define tasking node urls
