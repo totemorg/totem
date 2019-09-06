adadc7f2e7b0f7f8f11fb8a06515d4be24b50cc5
acmesds@somemail.com <acmesds@nowhere.org>
2019-09-06 18:59:23 -0500

    whatever

--- totem.js
+++ totem.js
@@ -33,6 +33,9 @@ var
 	ENV = process.env,
 	
 	// NodeJS modules
+	CRYPTO = require("crypto"),
+	TLS = require("TLS)",
+				  
 	STREAM = require("stream"), 	// pipe-able streams
 	HTTP = require("http"),						//< http interface
 	HTTPS = require("https"),					//< https interface
@@ -708,9 +711,13 @@ var
 				});
 			}
 
+			function sha256(s) {
+				return CRYPTO.createHash('sha256').update(s).digest('base64');
+			}
+		
 			var 
-				url = path.startsWith("/") ? TOTEM.host.master + path : path,
-				opts = URL.parse(url),
+				//url = path.startsWith("/") ? TOTEM.host.master + path : path,
+				opts = URL.parse(path),
 				protocol = opts.protocol || "",
 				trace = this.trace,
 				retries = this.retries,
@@ -719,7 +726,7 @@ var
 			opts.rejectUnauthorized = false;
 			opts.agent = false;
 			opts.method = post ? "PUT" : "GET";
-			opts.port = opts.port ||  (protocol.endsWith("s:") ? 443 : 80);
+			// opts.port = opts.port ||  (protocol.endsWith("s:") ? 443 : 80);
 			// opts.cipher = " ... "
 			// opts.headers = { ... }
 			// opts.Cookie = ["x=y", ...]
@@ -791,16 +798,64 @@ var
 				case "https:":
 					opts.pfx = cert.pfx;
 					opts.passphrase = cert._pass;
+					/*
+					opts.checkServerIdentity = function(host, cert) {
+						// Make sure the certificate is issued to the host we are connected to
+						const err = TLS.checkServerIdentity(host, cert);
+						if (err) {
+							Log("tls error", err);
+							return err;
+						}
 
+						// Pin the public key, similar to HPKP pin-sha25 pinning
+						const pubkey256 = 'pL1+qb9HTMRZJmuC/bB/ZI9d302BYrrqiVuRyW+DGrU=';
+						if (sha256(cert.pubkey) !== pubkey256) {
+							const msg = 'Certificate verification error: ' + `The public key of '${cert.subject.CN}' ` + 'does not match our pinned fingerprint';
+							return new Error(msg);
+						}
+
+						// Pin the exact certificate, rather then the pub key
+						const cert256 = '25:FE:39:32:D9:63:8C:8A:FC:A1:9A:29:87:' + 'D8:3E:4C:1D:98:DB:71:E4:1A:48:03:98:EA:22:6A:BD:8B:93:16';
+						if (cert.fingerprint256 !== cert256) {
+							const msg = 'Certificate verification error: ' +
+							`The certificate of '${cert.subject.CN}' ` +
+							'does not match our pinned fingerprint';
+							return new Error(msg);
+						}
+
+						// This loop is informational only.
+						// Print the certificate and public key fingerprints of all certs in the
+						// chain. Its common to pin the public key of the issuer on the public
+						// internet, while pinning the public key of the service in sensitive
+						// environments.
+						do {
+							console.log('Subject Common Name:', cert.subject.CN);
+							console.log('  Certificate SHA256 fingerprint:', cert.fingerprint256);
+
+							hash = crypto.createHash('sha256');
+							console.log('  Public key ping-sha256:', sha256(cert.pubkey));
+
+							lastprint256 = cert.fingerprint256;
+							cert = cert.issuerCertificate;
+						} while (cert.fingerprint256 !== lastprint256);
+
+						};
+					*/
+					
+					opts.agent = false; //new HTTPS.Agent(opts);
+					
 					try {
 						var Req = HTTPS.request(opts, getResponse);
+						console.log('statusCode:', res.statusCode);
+						console.log('headers:', res.headers['public-key-pins']);	// Print the HPKP values
 					}
 					catch (err) {
+						Log("FETCH ERROR", err);
 						return cb( "" );
 					}
 
 					Req.on('error', err => {
-						//Log("FETCH FAIL", err);
+						Log("FETCH FAIL", err);
 						cb( "" );
 					});
 
