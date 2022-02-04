7e9fad3e02c51629067e3401c06b72f2a9fe88fa
totemstan <brian.d.james@comcast.net>
2022-02-04 00:49:21 -0500

    refactor urls and all connectors moved to enums

---
+++ config/pass.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export SERVICE_PASS=pass
+export ADMIN_PASS=pass
+export DECODE_PASS=pass
+export LOGIN_PASS=pass
+export ODBC_PASS=pass
+export FETCH_PASS=pass
+export ZIP_PASS=pass
+export GIT_PASS=pass
+export GOOGLEUSE=user:pass
+
+export KEY_LEXNEX=user:pass
+export KEY_GOOGLE=user:pass
+export KEY_MYSQL=user:pass
+export KEY_NEO4J=user:pass
+export KEY_TXMAIL=user:pass
+export KEY_RXMAIL=user:pass
+export KEY_GIT=user:pass
